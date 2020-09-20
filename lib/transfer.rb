0f478b2a9179ef923bf9afc6ddf1ec28c0327ec3
stacksonstack <staceycarrillo44@gmail.com>
2020-09-20 11:37:39 -0400

    completed with sisan

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,35 @@
+require 'pry'
 class Transfer
-  # your code here
+  attr_accessor :sender, :receiver, :amount, :status
+
+  def initialize (sender, receiver, amount, status = "pending")
+    @sender = sender
+    @receiver = receiver
+    @amount = amount
+    @status = status
+    
+  end
+
+  def valid?
+    sender.valid? && receiver.valid? 
+  end
+
+  def execute_transaction
+    if @status == "pending" && self.valid? && sender.balance - amount >= 0
+      sender.balance -= amount 
+      receiver.balance += amount
+      @status = "complete"
+    else 
+      @status = "rejected"
+      "Transaction rejected. Please check your account balance."
+    end
+  end
+
+  def reverse_transfer
+    if @status == "complete" && self.valid?
+      sender.balance += amount
+      receiver.balance -= amount
+      @status = "reversed"
+    end
+  end
 end
